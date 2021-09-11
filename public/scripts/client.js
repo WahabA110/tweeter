be229d419a55e99a7432ffbe40866785d696dc33
Wahab Alamin <wahabalamin@gmail.com>
2021-09-11 14:46:59 -0400

    Deleted the data array. Moved the functions into the document ready function. Created loadTweets function which gets the tweets from /tweets, empties the id tweet-container in the html file, then uses the renderTweets function with the data it gets from /tweets. Added 2 if statements to the submit event listener to check if the text provided to create a new tweet is above 140 characters or empty and returns a relevant alert function. Added to the promise that it will log the res, clear the textarea, use the function loadTweets, and reset the character counter to 140.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,81 +5,82 @@
  */
 
 // just hardcoded tweets to be used as a test
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
-
-// loops through the tweets passed in as the parameter, calls the createTweetElement function
-// for each tweet passed through, and adds it onto the id tweet-container which is in the body
-// section of index.html
-const renderTweets = function(tweets) {
-  for (let item of tweets) {
-    $('#tweet-container').append(createTweetElement(item));
-  }
-}
-
-// function takes in the data, and adds it to an html
-// format which will be returned.
-const createTweetElement = (tweetData) => {
-  let $tweet = `
-  <article class="tweetArticle">
-    <header class="currentUser">
-      <h5>${tweetData.user.name}</h5>
-      <h5>${tweetData.user.handle}</h5>
-    </header>
-    <p>
-      ${tweetData.content.text}
-    </p>
-    <div class="linebreak"></div>
-    <footer>
-      <span><b>${timeago.format(tweetData.created_at)}</b></span>
-      <div>
-        <i class="fas fa-flag"></i>
-        <i class="fas fa-retweet"></i>
-        <i class="fas fa-heart"></i>
-      </div>
-    </footer>
-  </article>`
-
-  return $tweet;
-}
 
 
 $(document).ready(function() {
 
+  const loadTweets = () => {
+    $.get('/tweets', function(tweetData) {
+      // take container specified in html and clear it out
+      $('#tweet-container').empty();
+      // also take form, clear that out as well
+      $('textarea').val("");
+      // then render the tweets
+      renderTweets(tweetData);
+    })
+  }
+
+  // we want to target your tweet-container then create 
+  // an on call, then reload loadTweets function. Then chain
+  // another function trigger then reload
+  $('#tweet-container').on('reload', loadTweets).trigger('reload');
+
+  const renderTweets = function(tweets) {
+    for (let item of tweets) {
+      $('#tweet-container').append(createTweetElement(item));
+    }
+  }
+
+  const createTweetElement = (tweetData) => {
+    let $tweet = `
+    <article class="tweetArticle">
+      <header class="currentUser">
+        <h5>${tweetData.user.name}</h5>
+        <h5>${tweetData.user.handle}</h5>
+      </header>
+      <p>
+        ${tweetData.content.text}
+      </p>
+      <div class="linebreak"></div>
+      <footer>
+        <span><b>${timeago.format(tweetData.created_at)}</b></span>
+        <div>
+          <i class="fas fa-flag"></i>
+          <i class="fas fa-retweet"></i>
+          <i class="fas fa-heart"></i>
+        </div>
+      </footer>
+    </article>`
+  
+    return $tweet;
+  }
+
+
   $('form').on('submit', (event) => {
-    event.preventDefault()
+    event.preventDefault();
+    const text = $('#tweet-text').val();
+    if (text.length > 140) {
+      alert('Character length is too long. Please shorten.');
+      return;
+    }
+    if (text.length === 0) {
+      alert('No text inputted. Please input before submitting.');
+      return;
+    }
     $.ajax({
       url: '/tweets',
       method: 'POST',
       data: $('form').serialize()
     })
     .then((res) => {
-      console.log(res);
+      console.log('res:', res);
+      $('textarea').val("");
+      loadTweets();
+      renderTweets(data);
+      $('.counter').val('140');
     })
-  })
-
+  });
+  loadTweets();
   renderTweets(data);
 
 });
